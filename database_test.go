8151413b286054445bb10695bdf516a1c0e39595
sfomuseumbot <sfomuseumbot@localhost>
2022-12-01 10:42:10 -0800

    initial commit

---
+++ database_test.go
@@ -0,0 +1,26 @@
+package mattn
+
+import (
+	"context"
+	"github.com/aaronland/go-sqlite/v2"
+	"testing"
+)
+
+func TestNewDatabase(t *testing.T) {
+
+	ctx := context.Background()
+
+	uri := "mattn://mem"
+
+	db, err := sqlite.NewDatabase(ctx, uri)
+
+	if err != nil {
+		t.Fatalf("Failed to create database for '%s', %v", uri, err)
+	}
+
+	dsn := db.DSN(ctx)
+
+	if dsn != "file::memory:?mode=memory&cache=shared" {
+		t.Fatalf("Invalid DSN string '%s'", dsn)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/aaronland/go-sqlite-mattn
+
+go 1.18
+
+require (
+	github.com/aaronland/go-sqlite/v2 v2.0.0
+	github.com/mattn/go-sqlite3 v1.14.16
+)
+
+require github.com/aaronland/go-roster v1.0.0 // indirect
